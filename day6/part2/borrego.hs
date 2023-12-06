32302910f3116d4df3c31ccb0ea90c0dbed9b8db
José Nuno Macedo <jose.n.macedo@inesctec.pt>
2023-12-06 11:09:55 +0000

    day6

---
+++ day6/part2/borrego.hs
@@ -0,0 +1,4 @@
+import Data.List
+
+main :: IO ()
+main = readFile "input.txt" >>= print . product . map (length . (\[t :: Int, d] -> [tc * tp | tc <- [1..t-1], let tp = t - tc, (tc * tp) > d]) . map read) . transpose . map (return . filter (/= ' ') . tail . dropWhile (/= ':')) . lines
